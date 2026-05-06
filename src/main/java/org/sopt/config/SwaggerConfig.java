23e4c11b3bb8cc0c8421a24fbaabdf935749ad5e
heveidndksjd <kayoonkim@naver.com>
2026-05-06 13:46:51 +0900

    feat: Swagger 설정에 배포 서버 추가

--- src/main/java/org/sopt/config/SwaggerConfig.java
+++ src/main/java/org/sopt/config/SwaggerConfig.java
@@ -2,15 +2,23 @@ package org.sopt.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI assignmentOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://beneruufin.store");
+        server.setDescription("배포 서버");
+
         return new OpenAPI()
+                .servers(List.of(server))
                 .info(new Info()
                         .title("Post API")
                         .description("게시글 API 명세서")
