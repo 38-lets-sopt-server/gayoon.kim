44426ced62dc613c999e7d131bca567a37191370
heveidndksjd <kayoonkim@naver.com>
2026-05-22 18:44:50 +0900

    feat: 인증 사용자 기반 게시글, 좋아요 처리

--- src/main/java/org/sopt/controller/LikeController.java
+++ src/main/java/org/sopt/controller/LikeController.java
@@ -2,42 +2,47 @@ package org.sopt.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.sopt.dto.request.LikeRequest;
 import org.sopt.dto.response.ApiResponse;
 import org.sopt.service.LikeService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Like", description = "좋아요 관련 API")
 @RestController
 @RequestMapping("/posts")
 public class LikeController {
+
     private final LikeService likeService;
 
     public LikeController(LikeService likeService) {
         this.likeService = likeService;
     }
 
-    @Operation(summary = "좋아요 추가", description = "사용자가 특정 게시글에 좋아요를 추가합니다.")
+    @Operation(summary = "좋아요 추가", description = "로그인한 사용자가 특정 게시글에 좋아요를 추가합니다.")
     @PostMapping("/{postId}/likes")
     public ResponseEntity<ApiResponse<Void>> addLike(
             @PathVariable Long postId,
-            @RequestBody LikeRequest request
+            Authentication authentication
     ) {
-        likeService.addLike(postId, request);
+        Long memberId = Long.parseLong(authentication.getName());
+
+        likeService.addLike(memberId, postId);
 
         return ResponseEntity.ok(
                 ApiResponse.success("좋아요 추가 완료!", null)
         );
     }
 
-    @Operation(summary = "좋아요 취소", description = "사용자가 특정 게시글에 누른 좋아요를 취소합니다.")
+    @Operation(summary = "좋아요 취소", description = "로그인한 사용자가 특정 게시글에 누른 좋아요를 취소합니다.")
     @DeleteMapping("/{postId}/likes")
     public ResponseEntity<ApiResponse<Void>> cancelLike(
             @PathVariable Long postId,
-            @RequestBody LikeRequest request
+            Authentication authentication
     ) {
-        likeService.cancelLike(postId, request);
+        Long memberId = Long.parseLong(authentication.getName());
+
+        likeService.cancelLike(memberId, postId);
 
         return ResponseEntity.ok(
                 ApiResponse.success("좋아요 취소 완료!", null)
